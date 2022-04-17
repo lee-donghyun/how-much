2556dbf808b178ad8ed4465f3d309eb62c74b3bc
lee dong hyun <donghyun.lee@fetching.co.kr>
2022-04-17 11:48:49 +0900

    통계 기능 구현

---
+++ src/pages/Stastics/helper.ts
@@ -0,0 +1,12 @@
+export enum FILTER {
+  ALL,
+  PLUS = "plus",
+  MINUS = "minus",
+}
+
+export enum SORT {
+  VALUE,
+  VALUE_DESC,
+  UNIX,
+  UNIX_DESC,
+}
--- src/pages/Stastics/index.tsx
+++ src/pages/Stastics/index.tsx
@@ -1,12 +1,25 @@
 import { DownOutlined, SettingOutlined } from "@ant-design/icons";
-import { Statistic, Button, message, Col, Row } from "antd";
+import {
+  Statistic,
+  Button,
+  message,
+  Row,
+  Radio,
+  Select,
+  Divider,
+  List,
+} from "antd";
 import dayjs from "dayjs";
 import { useLiveQuery } from "dexie-react-hooks";
 import { FC, useState } from "react";
+import Record from "../../components/Record";
 import db from "../../services/api/db";
+import { FILTER, SORT } from "./helper";
 
 const StasticsPage: FC = () => {
   const [date, setDate] = useState(dayjs());
+  const [filter, setFilter] = useState<FILTER>(FILTER.ALL);
+  const [sort, setSort] = useState<SORT>(SORT.UNIX);
 
   const records = useLiveQuery(
     () =>
@@ -16,7 +29,7 @@ const StasticsPage: FC = () => {
           date.clone().startOf("month").unix(),
           date.clone().add(1, "month").startOf("month").unix()
         )
-        .sortBy("unix"),
+        .toArray(),
     [date]
   );
   const [plus, minus] = records?.reduce(
@@ -63,26 +76,94 @@ const StasticsPage: FC = () => {
           loading={!records}
         />
       </div>
-      <Row gutter={20} style={{ padding: 20 }}>
-        <Col span={12}>
-          <Statistic
-            title="수입"
-            value={plus}
-            valueStyle={{ color: "#389e0d", fontSize: "22px" }}
-            suffix="원"
-            loading={!records}
-          />
-        </Col>
-        <Col span={12}>
-          <Statistic
-            title="지출"
-            value={minus}
-            valueStyle={{ color: "#cf1322", fontSize: "22px" }}
-            suffix="원"
-            loading={!records}
-          />
-        </Col>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr",
+          padding: "0 20px",
+        }}
+      >
+        <Statistic
+          title="수입"
+          value={plus}
+          valueStyle={{ color: "#389e0d", fontSize: "22px" }}
+          suffix="원"
+          loading={!records}
+        />
+        <Statistic
+          title="지출"
+          value={minus}
+          valueStyle={{ color: "#cf1322", fontSize: "22px" }}
+          suffix="원"
+          loading={!records}
+        />
+      </div>
+      <Divider style={{ marginBottom: 0 }} />
+      <Row
+        justify="space-between"
+        style={{
+          padding: "24px 20px",
+          position: "sticky",
+          top: 72,
+          background: "white",
+          zIndex: 10,
+        }}
+      >
+        <Radio.Group
+          value={filter}
+          onChange={({ target: { value } }) => setFilter(value)}
+        >
+          <Radio.Button value={FILTER.ALL}>전체</Radio.Button>
+          <Radio.Button value={FILTER.PLUS}>수입</Radio.Button>
+          <Radio.Button value={FILTER.MINUS}>지출</Radio.Button>
+        </Radio.Group>
+        <Select style={{ width: 120 }} value={sort} onChange={setSort}>
+          <Select.Option value={SORT.VALUE_DESC}>금액 높은 순</Select.Option>
+          <Select.Option value={SORT.VALUE}>금액 낮은 순</Select.Option>
+          <Select.Option value={SORT.UNIX_DESC}>최신 순</Select.Option>
+          <Select.Option value={SORT.UNIX}>오래된 순</Select.Option>
+        </Select>
       </Row>
+      <div
+        style={{
+          paddingBottom: "calc(env(safe-area-inset-bottom) + 80px)",
+        }}
+      >
+        <List
+          itemLayout="horizontal"
+          dataSource={records
+            ?.filter((record) =>
+              filter == FILTER.ALL ? true : record.type == filter
+            )
+            .sort((a, b) => {
+              switch (sort) {
+                case SORT.UNIX:
+                  return a.unix - b.unix;
+                case SORT.UNIX_DESC:
+                  return b.unix - a.unix;
+                case SORT.VALUE:
+                  return a.value - b.value;
+                case SORT.VALUE_DESC:
+                  return b.value - a.value;
+              }
+            })}
+          rowKey={(record) => record.id ?? ""}
+          renderItem={(record) => {
+            const date = dayjs(record.unix * 1000);
+            return (
+              <List.Item className="record" onClick={() => {}}>
+                <Record
+                  mode={record.type}
+                  title={`${record.value.toLocaleString()}원`}
+                  description={`${record.description} - ${date.format(
+                    "YY/MM/DD"
+                  )}`}
+                />
+              </List.Item>
+            );
+          }}
+        />
+      </div>
     </div>
   );
 };
